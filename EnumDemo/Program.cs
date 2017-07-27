46c2807ebf743be3332f97df1421dc09bed22ed2
abhishek srivastava <abhi_sri_amity6@yahoo.com>
2017-07-27 13:52:10 +0530

    partilly completed

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -8,8 +8,35 @@ namespace EnumDemo
 {
     class Program
     {
+        enum Apple
+        {
+           Jonathan, GoldenDel, RedDel, Winesap,
+           Cortland, McIntosh
+        };
+
         static void Main(string[] args)
         {
+            string[] color = {  "Red",
+                                "Yellow",
+                                "Red",
+                                "Red",
+                                "Red",
+                                "Reddish Green"
+                              };
+            
+            Apple i;
+            for(i = Apple.Jonathan;i<Apple.McIntosh;i++)
+            {
+                Console.WriteLine(i + "has value " + (int)i);
+                
+            }
+            Console.WriteLine();
+            // Use an enumeration to index an array.
+            for (i = Apple.Jonathan; i <= Apple.McIntosh; i++)
+                Console.WriteLine("Color of " + i + " is " + color[(int)i]);
+            Console.Read();
+
+
         }
     }
 }
